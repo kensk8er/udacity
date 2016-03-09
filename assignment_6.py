474de9b5df215c1587d38979436480ac98e84eb4
kensk8er <kensk8er1017@gmail.com>
2016-03-09 01:20:25 +0000

    implemented bigram model.

--- assignment_6.py
+++ assignment_6.py
@@ -10,7 +10,6 @@ import numpy as np
 import random
 import string
 
-import pickle
 import tensorflow as tf
 import zipfile
 
@@ -29,6 +28,10 @@ NUM_NODES = 64
 NUM_STEPS = 7001
 SUMMARY_FREQUENCY = 100
 EMBEDDING_DIMENSION = 128
+
+CHARACTER_SIZE = (len(string.ascii_lowercase) + 1)  # [a-z] + ' '
+VOCABULARY_SIZE = CHARACTER_SIZE ** 2  # [a-z] + ' ' (bigram)
+FIRST_LETTER = ord(string.ascii_lowercase[0])
 
 
 def maybe_download(filename, expected_bytes):
@@ -55,7 +58,7 @@ def read_data(filename):
 
 def char2id(char):
     if char in string.ascii_lowercase:
-        return ord(char) - first_letter + 1
+        return ord(char) - FIRST_LETTER + 1
     elif char == ' ':
         return 0
     else:
@@ -63,11 +66,29 @@ def char2id(char):
         return 0
 
 
-def id2char(dict_id):
-    if dict_id > 0:
-        return chr(dict_id + first_letter - 1)
+def bigram2id(bigram):
+    """easily extensible to ngram2id actually"""
+    assert len(bigram) == 2, 'Input needs to be 2 characters.'
+    char_id = 0
+
+    for digit, char in enumerate(bigram):
+        char_id += char2id(char) * (CHARACTER_SIZE ** digit)
+
+    return char_id
+
+
+def id2char(char_id):
+    if char_id > 0:
+        return chr(char_id + FIRST_LETTER - 1)
     else:
         return ' '
+
+
+def id2bigram(char_id):
+    first_digit_id = char_id % CHARACTER_SIZE
+    second_digit_id = char_id // CHARACTER_SIZE
+
+    return id2char(first_digit_id) + id2char(second_digit_id)
 
 
 class BatchGenerator(object):
@@ -82,11 +103,11 @@ class BatchGenerator(object):
 
     def _next_batch(self):
         """Generate a single batch from the current cursor position in the data."""
-        batch = np.zeros(shape=(self._batch_size, vocabulary_size), dtype=np.float)
+        batch = np.zeros(shape=(self._batch_size, VOCABULARY_SIZE), dtype=np.float)
 
         for batch_id in range(self._batch_size):
-            batch[batch_id, char2id(self._text[self._cursor[batch_id]])] = 1.0
-            self._cursor[batch_id] = (self._cursor[batch_id] + 1) % self._text_size
+            batch[batch_id, bigram2id(self._text[self._cursor[batch_id]: self._cursor[batch_id] + 2])] = 1.0
+            self._cursor[batch_id] = (self._cursor[batch_id] + 1) % (self._text_size - 1)
 
         return batch
 
@@ -108,6 +129,12 @@ def characters(probabilities):
     """Turn a 1-hot encoding or a probability distribution over the possible
     characters back into its (most likely) character representation."""
     return [id2char(char_id) for char_id in np.argmax(probabilities, 1)]
+
+
+def bigrams(probabilities):
+    """Turn a 1-hot encoding or a probability distribution over the possible
+    bigrams back into its (most likely) bigram representation."""
+    return [id2bigram(bigram_id) for bigram_id in np.argmax(probabilities, 1)]
 
 
 def batches2string(batches):
@@ -143,15 +170,20 @@ def sample_distribution(distribution):
 
 def sample(prediction):
     """Turn a (column) prediction into 1-hot encoded samples."""
-    p = np.zeros(shape=[1, vocabulary_size], dtype=np.float)
+    p = np.zeros(shape=[1, VOCABULARY_SIZE], dtype=np.float)
     p[0, sample_distribution(prediction[0])] = 1.0
     return p
 
 
 def random_distribution():
     """Generate a random column of probabilities."""
-    b = np.random.uniform(0.0, 1.0, size=[1, vocabulary_size])
+    b = np.random.uniform(0.0, 1.0, size=[1, VOCABULARY_SIZE])
     return b / np.sum(b, 1)[:, None]
+
+
+def bigram_label2unigram_label(bigram_one_hot_encodings, batch_size=BATCH_SIZE):
+    unigram_id_labels = np.where(bigram_one_hot_encodings == 1)[1] // CHARACTER_SIZE
+    return np.array([[float(char_id == unigram_id_labels[batch_id]) for char_id in range(CHARACTER_SIZE)] for batch_id in range(batch_size)])
 
 
 if __name__ == '__main__':
@@ -161,9 +193,6 @@ if __name__ == '__main__':
     valid_text = text[:VALID_SIZE]
     train_text = text[VALID_SIZE:]
     train_size = len(train_text)
-
-    vocabulary_size = len(string.ascii_lowercase) + 1  # [a-z] + ' '
-    first_letter = ord(string.ascii_lowercase[0])
 
     train_batches = BatchGenerator(train_text, BATCH_SIZE, NUM_UNROLLINGS)
     valid_batches = BatchGenerator(valid_text, 1, 1)
@@ -180,11 +209,11 @@ if __name__ == '__main__':
         previous_state = tf.Variable(tf.zeros([BATCH_SIZE, NUM_NODES]), trainable=False)
 
         # Classifier weights and biases.
-        W = tf.Variable(tf.truncated_normal([NUM_NODES, vocabulary_size], -0.1, 0.1))
-        b = tf.Variable(tf.zeros([vocabulary_size]))
+        W = tf.Variable(tf.truncated_normal([NUM_NODES, CHARACTER_SIZE], -0.1, 0.1))
+        b = tf.Variable(tf.zeros([CHARACTER_SIZE]))
 
         # embedding
-        embeddings = tf.Variable(tf.random_uniform([vocabulary_size, EMBEDDING_DIMENSION], minval=-1.0, maxval=1.0))
+        embeddings = tf.Variable(tf.random_uniform([VOCABULARY_SIZE, EMBEDDING_DIMENSION], minval=-1.0, maxval=1.0))
 
         # Definition of the cell computation.
         def lstm_cell(X, output, state):
@@ -194,7 +223,7 @@ if __name__ == '__main__':
             X_output = tf.concat(1, [X, output])
             all_logits = tf.matmul(X_output, W_lstm) + b_lstm
 
-            input_gate = tf.sigmoid(all_logits[:,  NUM_NODES])
+            input_gate = tf.sigmoid(all_logits[:, :NUM_NODES])
             forget_gate = tf.sigmoid(all_logits[:, NUM_NODES: NUM_NODES * 2])
             output_gate = tf.sigmoid(all_logits[:, NUM_NODES * 2: NUM_NODES * 3])
             temp_state = all_logits[:, NUM_NODES * 3:]
@@ -208,7 +237,7 @@ if __name__ == '__main__':
         train_labels = list()
         for _ in range(NUM_UNROLLINGS):
             train_X.append(tf.placeholder(tf.int32, shape=[BATCH_SIZE, 1]))
-            train_labels.append(tf.placeholder(tf.float32, shape=[BATCH_SIZE, vocabulary_size]))
+            train_labels.append(tf.placeholder(tf.float32, shape=[BATCH_SIZE, CHARACTER_SIZE]))
 
         # Unrolled LSTM loop.
         outputs = list()
@@ -261,7 +290,7 @@ if __name__ == '__main__':
 
             for batch_id in range(NUM_UNROLLINGS):
                 feed_dict[train_X[batch_id]] = np.where(batches[batch_id] == 1)[1].reshape((-1, 1))
-                feed_dict[train_labels[batch_id]] = batches[batch_id + 1]
+                feed_dict[train_labels[batch_id]] = bigram_label2unigram_label(batches[batch_id + 1])
 
             _, l, predictions, lr = session.run([optimizer, loss, train_prediction, learning_rate], feed_dict=feed_dict)
 
@@ -275,7 +304,7 @@ if __name__ == '__main__':
                 print('Average loss at step %d: %f learning rate: %f' % (step, mean_loss, lr))
 
                 mean_loss = 0
-                labels = np.concatenate(list(batches)[1:])
+                labels = np.concatenate([bigram_label2unigram_label(batch) for batch in batches[1:]])
                 print('Minibatch perplexity: %.2f' % float(np.exp(log_prob(predictions, labels))))
 
                 if step % (SUMMARY_FREQUENCY * 10) == 0:
@@ -284,7 +313,7 @@ if __name__ == '__main__':
 
                     for _ in range(5):
                         feed = sample(random_distribution())
-                        sentence = characters(feed)[0]
+                        sentence = bigrams(feed)[0]
                         reset_sample_state.run()
 
                         for _ in range(79):
@@ -292,6 +321,8 @@ if __name__ == '__main__':
                             prediction = sample_prediction.eval({sample_input: feed})
                             feed = sample(prediction)
                             sentence += characters(feed)[0]
+                            last_bigram_id = bigram2id(sentence[-2:])
+                            feed = np.array([[float(last_bigram_id == bigram_id) for bigram_id in range(VOCABULARY_SIZE)]])
 
                         print(sentence)
 
@@ -304,6 +335,6 @@ if __name__ == '__main__':
                 for _ in range(VALID_SIZE):
                     valid_batch = valid_batches.next()
                     predictions = sample_prediction.eval({sample_input: np.where(valid_batch[0] == 1)[1].reshape((-1, 1))})
-                    valid_log_prob = valid_log_prob + log_prob(predictions, valid_batch[1])
+                    valid_log_prob = valid_log_prob + log_prob(predictions, bigram_label2unigram_label(valid_batch[1], batch_size=1))
 
                 print('Validation set perplexity: %.2f' % float(np.exp(valid_log_prob / VALID_SIZE)))
